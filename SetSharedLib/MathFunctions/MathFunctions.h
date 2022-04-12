e2c93d1bf4e31d61428c1113ad97c5ef6a2842fc
w-boom <2506259430@qq.com>
2022-04-12 14:26:11 +0800

    增加静态库、动态库的开关项

--- SetSharedLib/MathFunctions/MathFunctions.h
+++ SetSharedLib/MathFunctions/MathFunctions.h
@@ -1,6 +1,10 @@
-#if define(MathFunctions_EXPORTS)
-	#define MathFunctions_API __declspec(dllexport)
+#if defined(BUILD_SHARED_LIBS)
+	#if define(MathFunctions_EXPORTS)
+		#define MathFunctions_API __declspec(dllexport)
+	#else
+		#define MathFunctions_API __declspec(dllimport)
+	#endif
 #else
-	#define MathFunctions_API __declspec(dllimport)
+	#define MathFunctions_API
 #endif
 
